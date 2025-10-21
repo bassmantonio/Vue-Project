1603f6ac425b81be01dd6e763ffd8e421122560f
Mauricio Moreno Marulanda <morenomarulanda.5@nlsa.teleperformance.com>
2025-10-21 12:02:51 -0500

    Created counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ counter }}</p>
+  <button @click="increase">Increase</button>
+  <br></br>
+  <button @click="decrease">Decrease</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const counter = ref(0);
+const increase = () =>
+{
+  counter.value++;
+}
+const decrease = () =>
+{
+  counter.value--;
+}
+</script>
+
+<style scoped>
+p{
+  color: blueviolet
+}
+</style>
