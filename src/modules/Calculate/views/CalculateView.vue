e5a96b52d355209195bfef69e7188994859a62ca
Mauricio Moreno Marulanda <morenomarulanda.5@nlsa.teleperformance.com>
2025-12-01 00:42:55 -0500

    v-if and v-show directives

--- src/modules/Calculate/views/CalculateView.vue
+++ src/modules/Calculate/views/CalculateView.vue
@@ -3,7 +3,11 @@
   <h3>Computed Properties </h3>
   <input type="text" v-model="value">
   {{ calculatedValue }}
-  <SectionResult :result="calculatedValue"></SectionResult>
+  <SectionResult v-if="showElement" :result="calculatedValue"></SectionResult>
+  <div v-else-if="isNaN(value)"> please text a number </div>
+  <div v-else>
+    here is showing nothing
+  </div>
 </template>
 
 <script setup>
@@ -13,6 +17,10 @@ import SectionResult from '../components/SectionResult.vue';
 const value = ref('');
 const calculatedValue = computed(() =>{
     return value.value *3;
+});
+
+const showElement = computed(() =>{
+    return value.value >10;
 });
 
 </script>
